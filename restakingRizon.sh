65e5bc6dcd92871b82a30263938c802ea6b88b9a
SPARMIT <82678602+5PARM1T@users.noreply.github.com>
2021-08-08 21:23:33 +0200

    Update restakingRizon.sh
    for RIZON TestNet v2 "GROOT-011"

--- restakingRizon.sh
+++ restakingRizon.sh
@@ -5,15 +5,15 @@ NC='\033[0m' # No Color
 DELEGATOR='Your delegator address'
 VALIDATOR='Your validator address'
 PASWD='password from cli'
-DELAY=3600 #in secs - how often restart the script 
-ACC_NAME=YourWalleName
+DELAY=3200 #in secs - how often restart the script 
+ACC_NAME=YourWalletName
 NODE=http://localhost:26657 #change it only if you use another rpc port of your node
 
 for (( ;; )); do
         BAL=$(rizond query bank balances ${DELEGATOR} --node ${NODE});
         echo -e "BALANCE: ${GREEN}${BAL}${NC} uatolo\n"
         echo -e "Claim rewards\n"
-        echo -e "${PASWD}\n${PASWD}\n" | rizond tx distribution withdraw-rewards ${VALIDATOR} --chain-id groot-07 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 5000uatolo
+        echo -e "${PASWD}\n${PASWD}\n" | rizond tx distribution withdraw-rewards ${VALIDATOR} --chain-id groot-011 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 5000uatolo
         for (( timer=10; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED}%02d${NC} sec\r" $timer
@@ -22,7 +22,7 @@ for (( ;; )); do
         BAL=$(rizond query bank balances ${DELEGATOR} --node ${NODE} -o json | jq -r '.balances | .[].amount');
         echo -e "BALANCE: ${GREEN}${BAL}${NC} uatolo\n"
         echo -e "Stake ALL\n"
-        echo -e "${PASWD}\n${PASWD}\n" | rizond tx staking delegate ${VALIDATOR} ${BAL}uatolo --chain-id groot-07 --from ${ACC_NAME} --node ${NODE} -y --fees 5000uatolo
+        echo -e "${PASWD}\n${PASWD}\n" | rizond tx staking delegate ${VALIDATOR} ${BAL}uatolo --chain-id groot-011 --from ${ACC_NAME} --node ${NODE} -y --fees 5000uatolo
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED}%02d${NC} sec\r" $timer
