1e3f25a254b1cb8333558310c8d8587bfe29f2e1
Nikita Lysenko <nikitlysenko@gmail.com>
2022-10-11 15:00:10 +0300

    Update restakingRebus.sh

--- restakingRebus.sh
+++ restakingRebus.sh
@@ -6,37 +6,26 @@ DELEGATOR_ADDRESS='rebus1....address'
 VALIDATOR_ADDRESS='rebusvaloper....adress'
 PWD=YourPassword'
 DELAY=7200 #in secs - how often restart the script 
-ACC_NAME='web34ever' #example: = ACC_NAME=wallet_qwwq_54
+ACC_NAME='web34ever' #example: = ACC_NAME=wallet
 NODE=http://localhost:33657 #change it only if you use another rpc port of your node
 
 for (( ;; )); do
-        echo -e "Get reward from Delegation"
-        echo -e "${PWD}\ny\n" | rebusd tx distribution withdraw-all-rewards --from ${ACC_NAME} --fees 5000arebus --chain-id reb_1111-1 --yes
-        for (( timer=10; timer>0; timer-- ))
-        do
-                printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
-                sleep 1
-        done
-
-        BAL=$(rebusd query bank balances ${DELEGATOR_ADDRESS} --node ${NODE});
-        # BAL=$(($BAL -100000))
-        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} arebus\n"
         echo -e "Claim rewards\n"
         echo -e "${PWD}\n${PWD}\n" | rebusd tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id reb_1111-1 --from ${ACC_NAME} --node ${NODE} --commission -y --fees 10000arebus
-        for (( timer=10; timer>0; timer-- ))
+        for (( timer=4; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
                 sleep 1
         done
         BAL=$(rebusd query bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
-        BAL=$(($BAL -1000000))
+        BAL=$(echo "$BAL - 100000000"|bc)
         echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} arebus\n"
-        echo -e "Stake ALL 11111\n"
+        echo -e "Stake ALL \n"
         echo -e "${PWD}\n${PWD}\n" | rebusd tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}arebus --chain-id reb_1111-1 --from ${ACC_NAME} --node ${NODE} -y --gas-prices 0.01arebus --gas 3000000
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
                 sleep 1
-        done       
+        done
 
 done
